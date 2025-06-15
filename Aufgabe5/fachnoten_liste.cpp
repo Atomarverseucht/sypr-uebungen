2794b4a99d85106c386c5eda9d8d4a5c51434225
Atom <atomcraft.4u@web.de>
2025-06-15 21:47:05 +0200

    zwischenstand

--- Aufgabe5/fachnoten_liste.cpp
+++ Aufgabe5/fachnoten_liste.cpp
@@ -1,18 +1,52 @@
-#include "fachnoten_liste.h"
+#include "fachnoten_list.h"
 
-class fachnoten_liste::node{
-    public:
-        const fachnote* value;
-        node* next;
-        ~node(){
-            
-        }
+class fachnoten_liste::element final {
+  element *next;
+  fachnote *n;
+
+  element(element *e, fachnote *m) : next(e), n(m) {}
+
+  friend class fachnoten_liste;
+  friend class fachnoten_liste::iterator;
+};
+
+// Iterator-Funktionen
+fachnoten_liste::iterator::iterator(element *e) : current(e) {}
+
+bool fachnoten_liste::iterator::operator!=(const iterator &i) const {
+  return this->current != i.current;
 }
 
-fachnoten_liste::fachnoten_liste(delete_func del): deleter(del)
-{
-
+fachnote *&fachnoten_liste::iterator::operator*() const {
+  return this->current->n;
 }
 
+fachnoten_liste::iterator &fachnoten_liste::iterator::operator++() {
+  this->current = this->current->next;
+  return *this;
+}
 
+// fachnotenliste-Funktionen
+fachnoten_liste::fachnoten_liste() : head(nullptr) {}
 
+fachnoten_liste::~fachnoten_liste() {
+  element *e = this->head;
+  while (e != nullptr) {
+    element *x = e;
+    e = e->next;
+    delete x;
+  }
+}
+
+fachnoten_liste &fachnoten_liste::insert(fachnote *n) {
+  this->head = new element(this->head, n);
+  return *this;
+}
+
+fachnoten_liste::iterator fachnoten_liste::begin() {
+  return fachnoten_liste::iterator(this->head);
+}
+
+fachnoten_liste::iterator fachnoten_liste::end() {
+  return fachnoten_liste::iterator(nullptr);
+}
--- Aufgabe5/fachnoten_liste.h
+++ Aufgabe5/fachnoten_liste.h
@@ -8,6 +8,12 @@ class fachnoten_liste final{
         fachnoten_liste(delete_func del);
         node head;
 
+        // illegale Argumente
+        fachnoten_liste(const fachnoten_liste & ) = delete;
+        fachnoten_liste &operator=(const fachnoten_liste &) = delete;
+        fachnoten_liste(fachnoten_liste &&) = delete;
+        fachnoten_liste &operator=(fachnoten_liste &&) = delete;
+
     private:
         class node;
         delete_func deleter;
